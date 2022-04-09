0c52855e9fa2f2e92ee94426bce5de2953b71425
leoshli <leoshli@tencent.com>
2022-04-09 15:31:03 +0800

    feat: time to int64
    --story=869092167

--- conf/conf.go
+++ conf/conf.go
@@ -3,15 +3,15 @@ package conf
 // var ...
 var (
 	ResourceName string // 资源名
-	Verbose      bool // 是否开启日志输出
-	DaoMetrics   bool // 是否上报dao metrics
-	APIServer    bool // 是否生成 server 层代码
+	Verbose      bool   // 是否开启日志输出
+	DaoMetrics   bool   // 是否上报dao metrics
+	APIServer    bool   // 是否生成 server 层代码
 	StorageT     string // 存储层类型
 	TemplateDir  string // 模板文件目录
-	ProtoOnly    bool // 只生成协议
+	ProtoOnly    bool   // 只生成协议
 	AppName      string // app name
 	ServerName   string // sever name
-	CacheEnable  bool // 是否开启缓存
+	CacheEnable  bool   // 是否开启缓存
 )
 
 //StorageType ...
